0ad9bac67757a0916a5034df80057daab595b2dc
Manuel ADELE <manuel.adele@gmail.com>
2025-01-30 11:44:01 +0100

    WIP stat

---
+++ src/app/core/component/stat/stat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stat',
+  imports: [],
+  templateUrl: './stat.component.html',
+  styleUrl: './stat.component.scss'
+})
+export class StatComponent {
+
+}
