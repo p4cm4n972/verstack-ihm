d815bb69257b6f499f2e1a4bc6c005d44b0db13b
Manuel ADELE <manuel.adele@gmail.com>
2025-03-20 23:49:21 +0100

    WIP articles component

--- src/app/core/component/news/news.component.ts
+++ src/app/core/component/news/news.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { ArticlesService } from '../../../services/articles.service';
-import { MatOption, MatSelect } from '@angular/material/select';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 
 interface Article {
   title: string;
@@ -26,16 +31,24 @@ interface Article {
     MatButtonModule,
     MatChipsModule,
     MatGridListModule,
+    MatButtonModule,
   ],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss',
 })
 export class NewsComponent implements OnInit {
-  frameworks: string[] = ['Tous', 'Angular', 'Vue.js', 'React', 'SwiftUI', 'Svelte', 'Next.js'];
+  readonly dialog = inject(MatDialog);
+  defaultPicture: string = 'assets/images/bkg-home-old.jpg';
+
+  frameworks: string[] = ['tous', 'article', 'release', 'story'];
+  selectedDomainIndex: number = 0;
   articles: any[] = [];
   filteredArticles: any[] = [];
 
-  constructor(private articlesService: ArticlesService) {}
+  constructor(
+    private articlesService: ArticlesService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     this.loadArticles();
@@ -47,26 +60,48 @@ export class NewsComponent implements OnInit {
         this.articles = data;
         this.filteredArticles = data;
       },
-      error: (err) => console.error('Erreur lors du chargement des articles : ', err)
+      error: (err) =>
+        console.error('Erreur lors du chargement des articles : ', err),
     });
   }
-  
+
   // Recherche simple
   searchArticles(keyword: string): void {
-    this.filteredArticles = this.articles.filter(article =>
-      article.title.toLowerCase().includes(keyword.toLowerCase()) ||
-      article.excerpt.toLowerCase().includes(keyword.toLowerCase())
+    this.filteredArticles = this.articles.filter(
+      (article) =>
+        article.title.toLowerCase().includes(keyword.toLowerCase()) ||
+        article.excerpt.toLowerCase().includes(keyword.toLowerCase())
     );
   }
 
   // Filtre par framework
-  filterByFramework(framework: string): void {
-    this.filteredArticles = framework === 'Tous'
-      ? this.articles
-      : this.articles.filter(article => article.framework === framework);
+  filterByFramework(index: number): void {
+    this.filteredArticles =
+      index === 0
+        ? this.articles
+        : this.articles.filter(
+            (article) => article.category === this.frameworks[index]
+          );
   }
 
-  openArticle(elm: any) {}
+  openArticleContentDialog(content: any) {
+    let rendu = this.sanitizer.bypassSecurityTrustHtml(content);
+    const dialogRef = this.dialog.open(DialogDataArticle, {
+      data: rendu,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      // console.log(`Dialog result: ${result}`);
+    });
+  }
 
   filterByTag(elm: any) {}
+}
+@Component({
+  selector: 'dialog-data-articles',
+  templateUrl: './dialog-data-articles.html',
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogDataArticle {
+  data = inject(MAT_DIALOG_DATA);
 }
