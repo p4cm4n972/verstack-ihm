c31187a25bc8a2548919ba06695d82a679b5fa51
Manuel ADELE <manuel.adele@gmail.com>
2025-06-11 18:50:23 +0200

    test: enhance service unit tests

--- src/app/services/jwt-interceptor.service.spec.ts
+++ src/app/services/jwt-interceptor.service.spec.ts
@@ -1,19 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 import { PLATFORM_ID } from '@angular/core';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
 import { JwtInterceptorService } from './jwt-interceptor.service';
 
 describe('JwtInterceptorService', () => {
-  let service: JwtInterceptorService;
+  let interceptor: JwtInterceptorService;
+
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: PLATFORM_ID, useValue: 'browser' }]
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: PLATFORM_ID, useValue: 'browser' },
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true }
+      ]
     });
-    service = TestBed.inject(JwtInterceptorService);
+    interceptor = TestBed.inject(JwtInterceptorService);
+    http = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+    localStorage.clear();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should add Authorization header', () => {
+    localStorage.setItem('access_token', 'token');
+    http.get('/data').subscribe();
+    const req = httpMock.expectOne('/data');
+    expect(req.request.headers.has('Authorization')).toBeTrue();
+    expect(req.request.headers.get('Authorization')).toBe('Bearer token');
   });
 });
--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(UsersService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch all users', () => {
+    service.getAllUsers().subscribe();
+    const req = httpMock.expectOne('api/users');
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should post user', () => {
+    service.createUser({ name: 'John' }).subscribe();
+    const req = httpMock.expectOne('api/users');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.headers.get('Content-Type')).toBe('application/json');
+  });
+
+  it('should delete user', () => {
+    service.deleteUser('1').subscribe();
+    const req = httpMock.expectOne('api/users/1');
+    expect(req.request.method).toBe('DELETE');
+  });
+
+  it('should patch user favoris', () => {
+    service.update('2', ['a']).subscribe();
+    const req = httpMock.expectOne('api/users/2');
+    expect(req.request.method).toBe('PATCH');
+    expect(req.request.body).toEqual({ favoris: ['a'] });
   });
 });
