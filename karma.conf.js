8c8eb2bf1cdba6e63ef931e1873708dd74378000
P4cm4n972 <manuel.adele@gmail.com>
2025-06-11 23:17:17 +0200

    test firefox launcher for codex

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -21,7 +21,7 @@ module.exports = function (config) {
       reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['FirefoxHeadless'],
     singleRun: true,
     restartOnFileChange: true
   });
