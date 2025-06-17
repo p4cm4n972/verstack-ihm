47afa2b2edcd3736ed218b2d89c45e5748d7b8f1
Manuel ADELE <manuel.adele@gmail.com>
2025-06-17 21:57:21 +0200

    refactor(server): adjust prerender routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,21 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'home', renderMode: RenderMode.Prerender },
+  { path: 'about', renderMode: RenderMode.Prerender },
+  { path: 'version', renderMode: RenderMode.Prerender },
+  { path: 'stat', renderMode: RenderMode.Prerender },
+  { path: 'shop', renderMode: RenderMode.Prerender },
+  { path: 'news', renderMode: RenderMode.Prerender },
+  { path: 'signup', renderMode: RenderMode.Prerender },
+  { path: 'mentions', renderMode: RenderMode.Prerender },
+  { path: 'privacy-policy', renderMode: RenderMode.Prerender },
+  { path: 'release', renderMode: RenderMode.Prerender },
+  { path: 'mobile-not-allowed', renderMode: RenderMode.Prerender },
+  { path: 'manifeste', renderMode: RenderMode.Prerender },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
